34d8615d8c83be9e4fefa8fbe316c2cbed6dd83e
Isaac Lee <legoae86@gmail.com>
2025-12-09 16:15:44 -0500

    g

--- backend/tests/test_state_estimation.py
+++ backend/tests/test_state_estimation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from state_estimation import IMUSLAMFusion
+from .state_estimation import IMUSLAMFusion
 import cv2
 import time
 
@@ -21,14 +21,20 @@ GyroY(deg/s)
 GyroZ(deg/s)
 """
 
-
-
 def test_imu_slam_fusion():
     fusion = IMUSLAMFusion()
     i = 0
     while(true):
         i = i + 1
-        time = df['TIMESTAMP'].dloc[i]
+        imu = IMNUData(
+            ax = df['AccelX(m/s^2)'][i], ay = df['AccelY(m/s^2)'][i], az = df['AccelZ(m/s^2)'][i],
+            gx = df['GyroX(deg/s)'][i], gy = df['GyroY(deg/s)'][i], gz = df['GyroZ(deg/s)'][i],
+            timestamp = df['TIMESTAMP'][i]
+        )
+        fusion.predict(imu)
+        updated_pose = fusion.update()
+        print("updated Orientation: ", updated_pose.qw, updated_pose.qx, updated_pose.qy, updated_pose.qz)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
